8e9624eb639d56b1a41603a663e5616c7747813c
martinMamani <mpm.mamani@gmail.com>
2021-03-04 18:02:22 -0300

    Se modifico el componente App y se agrego una imagen a assets

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import imageCat from './assets/cat.png';
 
-export default function App() {
+
+const App = () => {
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Text style={styles.title}>Hello Word</Text>
+      <Image
+        source={{ uri: 'https://picsum.photos/200/200' }}
+        //source={imageCat}
+        style={styles.image}>
+      </Image>
+      {/* <Button
+        title='hola'
+        color='red'
+        //onPress={() => console.log("hello")}
+        onPress={() => Alert.alert('Boom! ')}
+      >
+      </Button> */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert("Boom")}
+      >
+        <Text style={styles.buttonText}>Press Me</Text>
+      </TouchableOpacity>
+    </View >
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#292929' },
+  title: { fontSize: 30, color: '#fff' },
+  image: { height: 200, width: 200, borderRadius: 100 },
+  button: { backgroundColor: 'deepskyblue', padding: 7, marginTop: 10, },
+  buttonText: { color: '#fff', fontSize: 20 }
+})
+
+export default App;
